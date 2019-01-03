f419ca4652bc476a63f3d100375829fecd14cb26
snkoski <shawnnkoski@gmail.com>
2019-01-03 12:04:54 -0500

    Display posts and add older posts/newer posts links to '/index', '/user', and '/explore' pages. Change posts per page to 25.

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dictatorcracker@gmail.com']
+    POSTS_PER_PAGE = 25
